653e91b36c1223cdcfe684891762dc4c99162efb
Cemalettin Yılmaz <95696669+cemalettinyilmaz@users.noreply.github.com>
2022-04-21 20:17:06 +0300

    Create Day Of Programmer.cs

---
+++ Day Of Programmer.cs
@@ -0,0 +1,56 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static string dayOfProgrammer(int year)
+    {
+        
+        if(year%400==0 || (year%4==0 && year%100!=0 )|| year==1800 || year==1900)
+        {
+            return "12.09."+year;
+        }
+   
+        
+
+
+        return "13.09."+year;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.dayOfProgrammer(year);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
